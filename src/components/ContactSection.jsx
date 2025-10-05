415b9a55553c138cf19c3a9ee4ab3fe4797eeab1
sair-gp <sair.bermudez@protonmail.com>
2025-10-05 19:30:07 -0400

    Centering the contact me section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -22,133 +22,82 @@ export const ContactSection = () => {
           Have a project in mind or want to collaborate? Feel free to reach out.
           I'm always open to discussing new opportunities.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-          <div className="space-y-8">
-            <h3 className="text-2xl font-semibold mb-6">
-              {" "}
-              Contact Information
-            </h3>
-            <div className="space-y-6 justify-center">
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bd-primary/10">
+        <div className="flex flex-col items-center justify-center px-4 py-12">
+          <div className="max-w-md w-full space-y-10 text-center">
+            <h3 className="text-2xl font-semibold">Contact Information</h3>
+
+            <div className="space-y-8">
+              {/* Email */}
+              <div className="flex flex-col sm:flex-row items-center sm:items-center sm:justify-start sm:space-x-4 lg:ml-20 md:ml-20">
+                <div className="p-3 rounded-full bg-primary/10 flex-shrink-0">
                   <Mail className="h-6 w-6 text-primary" />
                 </div>
-                <div>
-                  <h4 className="font-medium"> Email</h4>
+                <div className="text-center sm:text-left mt-2 sm:mt-0">
+                  <h4 className="font-medium">Email</h4>
                   <a
                     href="mailto:sair.bermudez@protonmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    {" "}
                     sair.bermudez@protonmail.com
                   </a>
                 </div>
               </div>
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bd-primary/10">
+
+              {/* Phone */}
+              <div className="flex flex-col sm:flex-row items-center sm:items-center sm:justify-start sm:space-x-4 lg:ml-20 md:ml-20">
+                <div className="p-3 rounded-full bg-primary/10 flex-shrink-0">
                   <Phone className="h-6 w-6 text-primary" />
                 </div>
-                <div>
-                  <h4 className="font-medium ml-14"> Phone</h4>
+                <div className="text-center sm:text-left mt-2 sm:mt-0">
+                  <h4 className="font-medium">Phone</h4>
                   <a
-                    href="mailto:sair.bermudez@protonmail.com"
-                    className="text-muted-foreground hover:text-primary transition-colors ml-13"
+                    href="tel:+59896247344"
+                    className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    {" "}
                     +598 96 247 344
                   </a>
                 </div>
               </div>
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bd-primary/10">
+
+              {/* Location */}
+              <div className="flex flex-col sm:flex-row items-center sm:items-center sm:justify-start sm:space-x-4 lg:ml-20 md:ml-20">
+                <div className="p-3 rounded-full bg-primary/10 flex-shrink-0">
                   <MapPin className="h-6 w-6 text-primary" />
                 </div>
-                <div>
-                  <h4 className="font-medium"> Location</h4>
-                  <a className="text-muted-foreground hover:text-primary transition-colors">
-                    {" "}
-                    Montevideo, Uruguay.
-                  </a>
+                <div className="text-center sm:text-left mt-2 sm:mt-0">
+                  <h4 className="font-medium">Location</h4>
+                  <p className="text-muted-foreground">Montevideo, Uruguay</p>
                 </div>
               </div>
             </div>
+
+            {/* Social Links */}
             <div className="pt-8">
-              <h4 className="font-medium mb-4"> Connect With Me</h4>
-              <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+              <h4 className="font-medium mb-4">Connect With Me</h4>
+              <div className="flex justify-center space-x-6">
+                <a
+                  href="#"
+                  target="_blank"
+                  className="hover:text-primary transition-colors"
+                >
                   <Linkedin />
                 </a>
-                <a href="#" target="_blank">
+                <a
+                  href="#"
+                  target="_blank"
+                  className="hover:text-primary transition-colors"
+                >
                   <Instagram />
                 </a>
-                <a href="#" target="_blank">
+                <a
+                  href="#"
+                  target="_blank"
+                  className="hover:text-primary transition-colors"
+                >
                   <Twitter />
                 </a>
               </div>
             </div>
-          </div>
-          <div className="bg-card p-8 rounded-lg shadow-xs">
-            <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
-            <form action="space-y-6">
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium mb-2"
-                >
-                  {" "}
-                  Your Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
-                  placeholder="Sair Bermudez..."
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium mb-2"
-                >
-                  {" "}
-                  Your Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
-                  placeholder="john@gmail.com"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-medium mb-2"
-                >
-                  {" "}
-                  Your Message
-                </label>
-                <textarea
-                  name="message"
-                  id="message"
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary resize-none"
-                  placeholder="Hello, I'd like to talk about..."
-                  required
-                />
-              </div>
-              <button
-                type="submit"
-                className={cn(
-                  "cosmic-button w-full flex items-center justify-center gap-2"
-                )}
-              >
-                <Send size={16} />
-              </button>
-            </form>
           </div>
         </div>
       </div>
